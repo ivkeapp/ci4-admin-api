8d932e88031f2336b7d886a5915a33e48297fa5b
Ivan Zarkovic <ivan.zarkovic@ewe.rs>
2024-08-01 11:17:09 +0200

    Add global loader for improved user experience

--- app/Views/admin/groups.php
+++ app/Views/admin/groups.php
@@ -34,8 +34,8 @@
         </div>
     </div>
 </div>
-<script src="vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="<?= site_url('vendor/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= site_url('vendor/datatables/dataTables.bootstrap4.min.js') ?>"></script>
 <script>
 $(document).ready(function() {
     $('#groupsTable').DataTable();
